de0bc6ab9d6b59a87549b2012a987fcb2fdd42fe
coeus-kashifaslam <kashif.aslam@coeus-solutions.de>
2016-12-29 12:23:47 +0500

    Selection Type script updated and default color update

--- source/js/ranking-scale-default.js
+++ source/js/ranking-scale-default.js
@@ -1,30 +1,44 @@
 $(document).ready(function (){
     var ratingList = $('.rating-comp__icons-list');
-    var ratingContinuous = $('.rating-comp__selection-type__continuous > li');
-    var ratingsingle = $('.rating-comp__selection-type__single > li');
-    var ratingListItem = $('.rating-comp__icons-list > li');
+    var ratingContinuous = $('.rating-comp__selection-type__continuous > li label');
+    var ratingsingle = $('.rating-comp__selection-type__single > li label');
+    var ratingListItem = $('.rating-comp__icons-list > li label');
+
     $(ratingListItem).hover(
         function() {
             var $this = $(this);
-            $this.addClass('rating-comp__hover rating-comp__hover__current');
-            $this.prevAll().addClass('rating-comp__hover');
+            $this.parent().addClass('rating-comp__hover rating-comp__hover__current');
+            $this.parent().prevAll().addClass('rating-comp__hover');
         }, function() {
             var $this = $(this);
-            $this.removeClass('rating-comp__hover rating-comp__hover__current');
-            $this.prevAll().removeClass('rating-comp__hover');
+            $this.parent().removeClass('rating-comp__hover rating-comp__hover__current');
+            $this.parent().prevAll().removeClass('rating-comp__hover');
         }
     );
 
     $(ratingContinuous).on('click', function () {
         var $this = $(this);
         $this.closest(ratingList).find('> li').removeClass('rating-comp__hover rating-comp__hover__current rating-comp__selected');
-        $this.addClass('rating-comp__selected');
-        $this.prevAll().addClass('rating-comp__selected');
+        $this.parent().addClass('rating-comp__selected');
+        $this.parent().prevAll().addClass('rating-comp__selected');
     });
 
     $(ratingsingle).on('click', function () {
         var $this = $(this);
         $this.closest(ratingList).find('> li').removeClass('rating-comp__hover rating-comp__hover__current rating-comp__selected');
-        $this.addClass('rating-comp__selected');
+        $this.parent().addClass('rating-comp__selected');
+    });
+
+    $(document).on('click','.rating-comp__selection-type__continuous.rating-comp__selection-type__total > li', function () {
+        var $this = $(this);
+        $this.closest(ratingList).find('> li').removeClass('rating-comp__hover rating-comp__hover__current rating-comp__selected');
+        $this.parent().addClass('rating-comp__selected');
+        $this.parent().prevAll().addClass('rating-comp__selected');
+    });
+
+    $(document).on('click', '.rating-comp__selection-type__single.rating-comp__selection-type__total > li' ,function () {
+        var $this = $(this);
+        $this.closest(ratingList).find('> li').removeClass('rating-comp__hover rating-comp__hover__current rating-comp__selected');
+        $this.parent().addClass('rating-comp__selected');
     });
 });
