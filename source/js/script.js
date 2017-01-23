0f4e6d27b9f50ce272e72d09daeec3b1dce29e38
coeus-kashifaslam <kashif.aslam@coeus-solutions.de>
2017-01-23 19:44:03 +0500

    images alt updated and loader added

--- source/js/script.js
+++ source/js/script.js
@@ -3,9 +3,21 @@ $(document).ready(function (){
         iconBase: 'ico',
         tickIcon: 'ico-tick'
     });
+
 });
 
 ;(function() {
     // Initialize
-    var bLazy = new Blazy();
+    var bLazy = new Blazy({
+        error: function(ele, msg){
+            if(msg === 'missing'){
+                $(ele).attr("src", "");
+                $(ele).attr("alt", "Image data-src is missing");
+            }
+            else if(msg === 'invalid'){
+                $(ele).attr("src", "");
+                $(ele).attr("alt", "Image not found");
+            }
+        }
+    });
 })();
