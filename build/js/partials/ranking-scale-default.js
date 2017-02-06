c40a3a134545fee07296f5876e9034f2d43f4ce8
coeus-kashifaslam <kashif.aslam@coeus-solutions.de>
2017-02-06 12:17:02 +0500

    HTML validation done

--- build/js/partials/ranking-scale-default.js
+++ build/js/partials/ranking-scale-default.js
@@ -1,8 +1,8 @@
 $(document).ready(function (){
     var ratingList = $('.rating-comp__icons-list');
-    var ratingContinuous = $('.rating-comp__selection-type__continuous > li label');
-    var ratingsingle = $('.rating-comp__selection-type__single > li label');
-    var ratingListItem = $('.no-touch .rating-comp__icons-list > li label');
+    var ratingContinuous = $('.rating-comp__selection-type__continuous > li .ranking__list__item__label');
+    var ratingsingle = $('.rating-comp__selection-type__single > li .ranking__list__item__label');
+    var ratingListItem = $('.no-touch .rating-comp__icons-list > li .ranking__list__item__label');
     $(ratingListItem).hover(
         function() {
             var $this = $(this);
@@ -27,13 +27,13 @@ $(document).ready(function (){
         $this.closest(ratingList).find('> li').removeClass('rating-comp__hover rating-comp__hover__current rating-comp__selected');
         $this.parent().addClass('rating-comp__selected');
     });
-    $(document).on('click','.rating-comp__selection-type__continuous.rating-comp__selection-type__total > li label', function () {
+    $(document).on('click','.rating-comp__selection-type__continuous.rating-comp__selection-type__total > li .ranking__list__item__label', function () {
         var $this = $(this);
         $this.closest(ratingList).find('> li').removeClass('rating-comp__hover rating-comp__hover__current rating-comp__selected');
         $this.parent().addClass('rating-comp__selected');
         $this.parent().prevAll().addClass('rating-comp__selected');
     });
-    $(document).on('click', '.rating-comp__selection-type__single.rating-comp__selection-type__total > li label' ,function () {
+    $(document).on('click', '.rating-comp__selection-type__single.rating-comp__selection-type__total > li .ranking__list__item__label' ,function () {
         var $this = $(this);
         $this.closest(ratingList).find('> li').removeClass('rating-comp__hover rating-comp__hover__current rating-comp__selected');
         $this.parent().addClass('rating-comp__selected');
