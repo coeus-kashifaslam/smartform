fbaeaeafba5607144d574a4362e658b1e94de5d2
coeus-kashifaslam <kashif.aslam@coeus-solutions.de>
2017-03-14 15:48:44 +0500

    Continuous rating thumbnail left fixed

---
+++ build/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function (){
+    $('.bootstrap-select').selectpicker({
+        iconBase: 'ico',
+        tickIcon: 'ico-tick'
+    });
+
+});
+
+;(function() {
+    // Initialize
+    var bLazy = new Blazy({
+        error: function(ele, msg){
+            if(msg === 'missing'){
+                $(ele).attr("src", "");
+                $(ele).attr("alt", "Image data-src is missing");
+            }
+            else if(msg === 'invalid'){
+                $(ele).attr("src", "");
+                $(ele).attr("alt", "Image not found");
+            }
+        }
+    });
+})();
