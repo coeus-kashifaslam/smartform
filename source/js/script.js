685cac3ffe0f687c8e46192c82bab56a034b53d4
coeus-kashifaslam <kashif.aslam@coeus-solutions.de>
2017-03-23 16:13:35 +0500

    product right thumbnai image fix

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,17 @@
 $(document).ready(function (){
-    $('.bootstrap-select').selectpicker({
-        iconBase: 'ico',
-        tickIcon: 'ico-tick'
-    });
+    // $('.bootstrap-select').selectpicker({
+    //     iconBase: 'ico',
+    //     tickIcon: 'ico-tick'
+    // });
+
+    var ratingHeight = parseInt($('.elem__rating-thumbnail-right__body .rating__icons__col__content').height()),
+        thumbnailHeight = parseInt($('.rating__thumbnail__col__content .rating__thumbnail__container .product-thumbnail').height());
+
+    if(ratingHeight <= thumbnailHeight ) {
+        $('.rating__thumbnail__col__content .rating__thumbnail__container .product-thumbnail').animate({
+            height: ratingHeight + 'px'
+        }, 1000);
+    }
 
 });
 
