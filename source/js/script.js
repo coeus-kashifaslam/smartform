3370e3c1a64baedd2c079c8fcaf2398e8da28e3b
Kashif <kashif.1992@yahoo.com>
2017-07-31 01:51:27 +0500

    Constant sum Component added

--- source/js/script.js
+++ source/js/script.js
@@ -29,3 +29,43 @@ $(document).ready(function (){
         }
     });
 })();
+
+
+// Range slider
+;(function () {
+    var $range = $(".js-range-slider"),
+        $input = $(".js-input"),
+        instance,
+        min = 0,
+        max = 100;
+
+    $range.ionRangeSlider({
+        type: "single",
+        min: min,
+        max: max,
+        from: 0,
+        onStart: function (data) {
+            $input.prop("value", data.from);
+        },
+        onChange: function (data) {
+            $input.prop("value", data.from);
+        }
+    });
+
+    instance = $range.data("ionRangeSlider");
+
+    $input.on("change keyup", function () {
+        var val = $(this).prop("value");
+
+        // validate
+        if (val < min) {
+            val = min;
+        } else if (val > max) {
+            val = max;
+        }
+
+        instance.update({
+            from: val
+        });
+    });
+})();
