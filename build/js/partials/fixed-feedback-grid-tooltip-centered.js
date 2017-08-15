e84d864f8237a3d2a1edaa6c728949f2b1e5eaf4
coeus-kashifaslam <kashif.aslam@coeus-solutions.de>
2017-08-15 19:07:36 +0500

    grid centered tooltip desktop version completed

---
+++ build/js/partials/fixed-feedback-grid-tooltip-centered.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    var gridTooltipRow = $('.fixed__grid-tooltip__row');
+    var $this = $(this);
+
+    if (gridTooltipRow.hasClass('fixed__grid-tooltip-centered')) {
+
+        var gridTooltipCentered = $('.fixed__grid-tooltip-centered'),
+
+        tableContentLastCol = gridTooltipCentered.next('.fixed__grid-table').find('.fixed__grid-content__row').find('.fixed__grid-table__last-col').outerWidth(),
+
+        tableShapesCount = gridTooltipCentered.next('.fixed__grid-table').find('.fixed__grid-content__row:first-child').find('.fixed__grid-shape__col').length,
+
+        tableShapesColWidth = gridTooltipCentered.next('.fixed__grid-table').find('.fixed__grid-content__row').find('.fixed__grid-shape__col').outerWidth(),
+
+
+        centerColWidth = tableShapesColWidth * tableShapesCount;
+        $this.find('.fixed__grid-table__last-col').width(tableShapesColWidth);
+        $this.find('.fixed__grid-table__middle-col').width(centerColWidth);
+    }
+});
