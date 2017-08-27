4bdf39aabad6f9db5afa8c7f93c93163e57527f3
Kashif <kashif.1992@yahoo.com>
2017-08-27 23:40:52 +0500

    two way binding added for boostrap range slider

--- build/js/script.js
+++ build/js/script.js
@@ -21,6 +21,13 @@ $(document).ready(function (){
             .val(slideEvt.value.newValue);
     });
 
+    $(document).on("change paste keyup",'.js-input', function(e) {
+        var $this = $(this),
+            inputVal = $this.val();
+        $this.parents('.elem__constant-sum__row')
+            .find('.bootstrap-slider').slider('setValue',inputVal);
+    });
+
 });
 
 ;(function() {
