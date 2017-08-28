86a9afa109de1ca7b89b061d083c7ebadfb30f8c
coeus-kashifaslam <kashif.aslam@coeus-solutions.de>
2017-08-28 13:58:20 +0500

    Constant sum total row added

--- build/js/script.js
+++ build/js/script.js
@@ -21,11 +21,12 @@ $(document).ready(function (){
             .val(slideEvt.value.newValue);
     });
 
-    $(document).on("change paste keyup",'.js-input', function(e) {
+    $('.js-input').on("change paste keyup", function(e) {
         var $this = $(this),
             inputVal = $this.val();
         $this.parents('.elem__constant-sum__row')
-            .find('.bootstrap-slider').slider('setValue',inputVal);
+            .find('.bootstrap-slider')
+            .slider('setValue',inputVal);
     });
 
 });
