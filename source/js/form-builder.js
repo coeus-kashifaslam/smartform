a719090eaac2dfa4da3df38d63d923396dcd7264
coeus-kashifaslam <kashif.aslam@coeus-solutions.de>
2017-10-23 15:16:41 +0500

    Tooltip added for form item actions

--- source/js/form-builder.js
+++ source/js/form-builder.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+
+    // Styled HTML 5 Select-Option Element
     $('.selectpicker').selectpicker();
+
+});
+
+
+
+// Init Bootstrap Tooltip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip({
+        placement: 'top'
+    });
 });
 
