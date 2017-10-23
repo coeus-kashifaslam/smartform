c13dfd9990b8ccd2e0ccbc67410441fb8eecf196
coeus-kashifaslam <kashif.aslam@coeus-solutions.de>
2017-10-23 18:47:21 +0500

    Modal base design added

--- build/js/form-builder.js
+++ build/js/form-builder.js
@@ -3,9 +3,10 @@ $(document).ready(function () {
     // Styled HTML 5 Select-Option Element
     $('.selectpicker').selectpicker();
 
+
+    $('.modal__form-builder').modal('show');
+
 });
-
-
 
 // Init Bootstrap Tooltip
 $(function () {
