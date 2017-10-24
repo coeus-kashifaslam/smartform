499de5c55167255228ababe38eaf33db2f85b362
coeus-kashifaslam <kashif.aslam@coeus-solutions.de>
2017-10-24 11:57:55 +0500

    New Color picker added

--- source/js/form-builder.js
+++ source/js/form-builder.js
@@ -3,10 +3,19 @@ $(document).ready(function () {
     // Styled HTML 5 Select-Option Element
     $('.selectpicker').selectpicker();
 
-
+    // Show Base Modal
     $('.modal__form-builder').modal('show');
 
+
+    // Init Color Picker
+    $(function() {
+        $('.colorpicker-component').colorpicker({
+            format: 'hex'
+        });
+    });
+
 });
+
 
 // Init Bootstrap Tooltip
 $(function () {
